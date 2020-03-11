39d905fbf8dcf5a409847e7e880b2900690a3f54
laundmo <laurinschmidt2001@gmail.com>
2020-03-11 11:00:02 +0100

    moved timout to file and deleted pycraft_test.py

---
+++ timeout_contextmanager.py
@@ -0,0 +1,25 @@
+import signal
+
+@contextmanager
+def timeout(time):
+    # Register a function to raise a TimeoutError on the signal.
+    signal.signal(signal.SIGALRM, raise_timeout)
+    # Schedule the signal to be sent after ``time``.
+    signal.alarm(time)
+
+    try:
+        yield
+    except TimeoutError:
+        pass
+    finally:
+        # Unregister the signal so it won't be triggered
+        # if the timeout is not reached.
+        signal.signal(signal.SIGALRM, signal.SIG_IGN)
+
+
+def raise_timeout(signum, frame):
+    raise TimeoutError("Took too long")
+
+with timeout(2):
+    pass
+
