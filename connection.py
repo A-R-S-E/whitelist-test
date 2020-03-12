f7b79ce8cfa445155217c690ccc3101ed3311356
laundmo <laurinschmidt2001@gmail.com>
2020-03-12 20:55:18 +0100

    getting closer to working

--- connection.py
+++ connection.py
@@ -4,25 +4,39 @@ from minecraft.networking.connection import Connection
 from minecraft.networking.packets.clientbound import play, login
 import sys
 
+def get_auth_token(username, password):
+    auth_token = authentication.AuthenticationToken()
+    try:
+        auth_token.authenticate(username, password)
+    except YggdrasilError as e:
+        print(e)
+    else:
+        return auth_token
+    return False
+
 class MinecraftConnection():
-    def __init__(self, ip, port, username, password):
+    def __init__(self, ip, port, username, password, auth_token=None):
         self.ip = ip
         self.port = port
         self.username = username
         self.password = password
-        self.auth_token = authentication.AuthenticationToken()
+        self.auth_token = authentication.AuthenticationToken() if auth_token is None else auth_token
+        self.skip_login = False if auth_token is None else True
         self.connection = None
-        self.success = None
+        self.success_packet = None
     
     def run(self):
-        if self.login():
+        login = True
+        if not self.skip_login:
+            login = self.login()
+        if login:
             if not self.password:
                 self.connection = Connection(self.ip, port=self.port, username=username)
             else:
                 self.connection = Connection(self.ip, port=self.port, auth_token=self.auth_token)
             self.register_packet_listeners()
             self.run_until_complete()
-        return self.success
+        return self.success_packet
 
     def login(self):
         if not self.password:
@@ -37,8 +51,8 @@ class MinecraftConnection():
     
     def run_until_complete(self):
         self.connection.connect()
-        self.success = "connecting"
-        while self.success == "connecting":
+        self.success_packet = "connecting"
+        while self.success_packet == "connecting":
             pass
     
     def register_packet_listeners(self):
@@ -58,13 +72,11 @@ class MinecraftConnection():
             )
     
     def handle_join_game(self, packet):
-        print('logged in: %r' % packet)
-        self.success = True
+        print('logged in: %r' % packet.__dict__)
+        self.success_packet = packet
         self.connection.disconnect()
 
     def handle_disconnect(self, packet):
         print('disconnected')
-        self.success = False
+        self.success_packet = False
         self.connection.disconnect()
-
-Connector("mc.salc1.com", 25565, "username", "password").run()
