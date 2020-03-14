cbad11574826f255408ff8a9d9290cebb33ae8fa
laundmo <laurinschmidt2001@gmail.com>
2020-03-14 15:35:50 +0100

    almost working

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import socket
 import sys
 import time
 
-
+from datetime import datetime
 import psycopg2
 from dotenv import load_dotenv
 from mcstatus import MinecraftServer
@@ -16,33 +16,42 @@ from minecraft.networking.packets import Packet, clientbound, serverbound
 import connection
 load_dotenv()
 
-class Server():
+
+class ServerTester():
     def __init__(self):
         self.conn = psycopg2.connect(database=os.environ['POSTGRES_DATABASE'], user=os.environ['POSTGRES_USER'],
                                 password=os.environ['POSTGRES_PASSWORD'], host=os.environ['POSTGRES_HOST'], port=os.environ['POSTGRES_PORT'])
-        self.server_id = server_id
-        self.auth_token = connection.get_auth_token("","")
+        self.conn.autocommit = True
+        self.server_id = None
+        self.auth_token = connection.get_auth_token("laurinschmidt2001@gmail.com","emiliala1")
 
-    def run_on_random(self):
-        self.randomize_server()
+    def run(self):
         if self.motd_ping():
             cracked = self.test_cracked()
             if not cracked:
                 self.test_whitelist()
 
+    def run_on_random(self):
+        self.randomize_server()
+        self.run()
+
     def test_cracked(self):
-        packet = connection.MinecraftConnection(*self.address, self.random_user, "").run()
+        username = self.random_user
+        packet = connection.MinecraftConnection(*self.address, username=username).run()
         if packet:
             self.write_packet(packet, cracked=True)
             return True
 
     def test_whitelist(self):
-        packet = connection.MinecraftConnection(*self.address, "", "", auth_token=self.auth_token).run()
+        packet = connection.MinecraftConnection(*self.address, auth_token=self.auth_token).run()
         if packet:
             self.write_packet(packet)
         
     def write_packet(self, packet, cracked=False):
-        pass
+        game_mode = packet.game_mode
+        level_type = packet.level_type
+        with self.conn.cursor() as c:
+            c.execute("""UPDATE public.servers SET last_checked=%s, world_type=%s, not_whitelisted=true, cracked=%s, gamemode=%s WHERE ip=%s AND port=%s;""", (datetime.now(), level_type, cracked, game_mode, *self.address))
     
     def randomize_server(self):
         with self.conn.cursor() as c:
@@ -54,19 +63,20 @@ class Server():
         with self.conn.cursor() as c:
             c.execute("SELECT u.username FROM users u INNER JOIN server_users su ON u.id = su.user_id WHERE su.server_id = %s ORDER BY random() LIMIT 1;", (self.server_id,))
             result = c.fetchone()
-            if len(result) > 0:
+            if result and len(result) > 0:
                 return result[0]
             else:
-                return
+                return "Server"
 
     @property
     def address(self):
         with self.conn.cursor() as c:
-            c.execute("select ip, port from servers where id = 766;", (self.server_id,))
+            c.execute("select ip, port from servers where id = %s;", (self.server_id,))
             ip, port = c.fetchone()
             return ip, port
 
     def motd_ping(self):
+        return True
         ipstr = "{}:{}".format(*self.address)
         try:
             status = MinecraftServer.lookup(ipstr).status(retries=2)
@@ -77,3 +87,7 @@ class Server():
                 return True
         finally:
             return False
+
+s = Server()
+s.server_id = 28650
+s.run()
